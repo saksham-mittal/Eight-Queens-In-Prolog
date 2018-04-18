2c7ae8111b5f246789fba0bf727c02054eb42214
saksham-mittal <mittalsaksham01@gmail.com>
2018-04-18 20:08:49 +0530

    Debugged code

--- test.pl
+++ test.pl
@@ -1,17 +1,31 @@
-:- initialization(solve(P)).
-/*
+:- initialization(input(P)).
+
 perm([X|Y], Z) :- perm(Y, W), takeout(X, Z, W).
 perm([], []).
 
 takeout(X, [X|R], R).
 takeout(X, [F|R], [F|S]) :- takeout(X, R, S).
 
-solve(P) :-
+input(P) :-
+     read_integer(A),
+     read_integer(B),
+     solve(P, B, A).
+
+solve(P, A, B) :-
      perm([1, 2, 3, 4, 5, 6, 7, 8], P),
+     check(P, [1, 2, 3, 4, 5, 6, 7, 8], A, B),
      combine([1, 2, 3, 4, 5, 6, 7, 8], P, S, D),
      all_diff(S),
      all_diff(D),
-     write_in_format(P, [1, 2, 3, 4, 5, 6, 7, 8]).
+     write_in_format(P),
+     !.
+
+check([X1|Y1], [X2|Y2], A, B) :-
+     X2 =:= A,
+     X1 =:= B.
+check([X1|Y1], [X2|Y2], A, B) :-
+     X2 < A,
+     check(Y1, Y2, A, B).
 
 combine([X1|X], [Y1|Y], [S1|S], [D1|D]) :-
      S1 is X1 + Y1,
@@ -21,13 +35,11 @@ combine([], [], [], []).
 
 all_diff([X|Y]) :-  \+member(X, Y), all_diff(Y).
 all_diff([X]).
-*/
-solve(P) :-
-     write_in_format([5, 2, 6, 1, 7, 4, 8, 3]).
 
 write_in_format([X|Y]) :-
      write_in_line(X, [1, 2, 3, 4, 5, 6, 7, 8]),
      write_in_format(Y).
+write_in_format([]).
 
 write_in_line(X, [A|B]) :-
    X =:= A,
